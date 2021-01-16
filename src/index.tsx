b6c896cd6f84b2e48512f921189865cddc93fd00
Marvin Trejo <42899019+marvintrejoxi@users.noreply.github.com>
2021-01-15 22:27:38 -0600

    ✨ setted up apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
+import {ApolloProvider} from '@apollo/client';
 
 // custom
+import reportWebVitals from './reportWebVitals';
+import {App} from './App';
+import {client} from 'graphql/client';
 import 'assets/sass/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
